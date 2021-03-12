d00c19291c2fb0138e6fdf1f06d691b7222a3868
MattCornicDev <glgharib@gmail.com>
2021-03-12 15:34:51 +0100

    Projet Robot C#

---
+++ Robot/Rover.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robot
+{
+    class Rover
+    {
+        private int port;
+        private string nomServeur;
+        private Byte commande;
+
+        public Rover(int port, string nomServeur, Byte commande)
+        {
+            this.port = port;
+            this.nomServeur = nomServeur;
+            this.Commande = commande;
+        }
+
+        public int Port { get => port; set => port = value; }
+        public string NomServeur { get => nomServeur; set => nomServeur = value; }
+        public Byte Commande { get => commande; set => commande = value; }
+    }
+
+   
+
+    public void Robot(string, int)
+    {
+
+    }
+    public void Commander(Byte[])
+    {
+        
+    }
+}
+*/
